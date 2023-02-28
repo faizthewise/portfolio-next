a37e93d77ad7124abcc12ecbdc46fc1fe58bd937
DESKTOP-RD29L48\thewise <faizhasnul97@gmail.com>
2023-02-28 22:55:49 +0800

    Add tailwind configuration

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["extends", "tailwind", "apply", "layer"],
+      },
+    ],
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global", "export"],
+      },
+    ],
+    "property-no-unknown": [true, { ignoreSelectors: [":export"] }],
+    "rule-empty-line-before": ["never", { ignore: ["after-comment"] }],
+    "selector-class-pattern": [
+      "^.[a-z]([a-zA-Z0-9]+)?((_([a-zA-Z0-9]+-?)+){0,2})?((__([a-zA-Z0-9]+-?)+){0,1})((_([a-zA-Z0-9]+-?)+){0,2})?$",
+      { resolveNestedSelectors: true },
+    ],
+  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -8,7 +10,50 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      mono: ["'Major\\ Mono \\ Display'", defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      fontFamily: {
+        display: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#EB6440",
+        secondary: "#497174",
+        tertiary: "#D6E4E5",
+        background: "#EFF5F5",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    // require("tailwindcss-animatecss")({
+    //   settings: {
+    //     animateSpeed: 250,
+    //   },
+    //   classes: [
+    //     "animate__animated",
+    //     "animate__infinite",
+    //     "animate__fast",
+    //     "animate__faster",
+    //     "animate__slow",
+    //     "animate__slower",
+    //     "animate__fadeIn",
+    //     "animate__fadeOut",
+    //     "animate__fadeInUp",
+    //     "animate__fadeInLeft",
+    //     "animate__fadeInRight",
+    //     "animate__fadeInDown",
+    //     "animate__fadeOutUp",
+    //     "animate__fadeOutDown",
+    //     "animate__fadeOutLeft",
+    //     "animate__fadeOutRight",
+    //     "animate__flipInX",
+    //     "animate__flipOutX",
+    //     "animate__flipInY",
+    //     "animate__flipOutY",
+    //   ],
+    // }),
+  ],
+};
